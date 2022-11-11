2e3b673e0b227203f0e89de81c92d7a314e99c8e
barispekeer <barispekeer@hotmail.com>
2022-11-11 12:22:01 +0300

    EnemyController added and a few edits

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject explosion;
+    public Image enemyHealt;
+    public Image panel;
+    float speed = 1f;
+    float bulletSpeed = 500f;
+    float healt = 1f;
+    public Transform player;
+
+    void Start()
+    {
+        
+            InvokeRepeating("EnemyFire", 0, 0.5f);
+        
+
+    }
+
+
+    void Update()
+    {
+        if (player)
+        {
+            if (transform.position.x < player.position.x)
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            if (transform.position.x > player.position.x)
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+            
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Destroy(collision.gameObject);
+            healt -= 0.05f;
+            if (healt <= 0)
+            {
+                Destroy(gameObject);
+                GameObject newExplosion = Instantiate(explosion, transform.position, Quaternion.identity);
+                Destroy(newExplosion, 1.5f);
+                panel.gameObject.SetActive(true);
+                Invoke("StopGame", 1f);
+            }
+            enemyHealt.fillAmount = healt;
+        }
+    }
+    void EnemyFire()
+    {
+        GameObject newBullet = Instantiate(bullet,transform.position,Quaternion.identity);
+        newBullet.GetComponent<Rigidbody2D>().AddForce(Vector2.down * bulletSpeed);
+        Destroy(newBullet, 1.5f);
+    }
+    void StopGame()
+    {
+        Time.timeScale = 0;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,10 @@ public class PlayerController : MonoBehaviour
     public GameObject playerBullet;
     public GameObject explosion;
     public Image playerHealt;
+    public Image panel;
     float playerSpeed = 5;
     float bulletSpeed = 500f;
-    float healt = 10f;
+    float healt = 1f;
     void Start()
     {
 
@@ -19,10 +20,13 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        Move();
+        Fire();
+    }
+   void Move()
+    {
         float yatay = Input.GetAxis("Horizontal");
         transform.Translate(yatay * Time.deltaTime * playerSpeed, 0, 0);
-
-        Fire();
     }
     void Fire()
     {
@@ -37,13 +41,21 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.tag.Equals("Respawn"))
         {
-            healt -= 1;
+            Destroy(collision.gameObject);
+            healt -= 0.1f;
             if (healt <= 0)
             {
+                Destroy(gameObject);
                 GameObject newExplosion = Instantiate(explosion, transform.position, Quaternion.identity);
                 Destroy(newExplosion, 1.5f);
+                panel.gameObject.SetActive(true);
+                Invoke("StopGame", 1f);
             }
             playerHealt.fillAmount = healt;
         }
+    }
+    void StopGame()
+    {
+        Time.timeScale = 0;
     }
 }
